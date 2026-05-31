6aabf1308d4c46e055811ad161cc44d5e82707ee
soyinkadgoat <soyinkadgreat807@gmail.com>
2026-05-31 14:22:01 +0100

    Add files via upload

---
+++ circumference.py
@@ -0,0 +1,7 @@
+def calculate_circumference(radius):
+    circumference = 2 * 22/7 * radius
+    print("The circumference of a circle is:", circumference)
+
+radius = int(input("Enter the radius of the circle: "))
+
+calculate_circumference(radius)
