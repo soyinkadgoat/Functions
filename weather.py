7774c7c650f70ddde901cb396e446f180d10ae95
soyinkadgoat <soyinkadgreat807@gmail.com>
2026-05-31 13:52:57 +0100

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,9 @@
+def weather(spring, autumn):
+
+    print("The weather is pleasant in:", spring)
+    print("The weather is the same in:", autumn)
+
+spring = "autumn"
+autumn = spring
+
+weather(spring, autumn)
